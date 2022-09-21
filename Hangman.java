75d2ac8b0f42eb48f0a728fc5669db9a1a5723d3
Adolfo Gonzalez <agonzal177@gmail.com>
2022-09-20 22:57:51 -0400

    added a check to game state

--- Hangman.java
+++ Hangman.java
@@ -11,26 +11,50 @@ public class Hangman{
         String[] wordArray= word.split("");
         String[] gameWord = getBlanks(word);
         String guessedLetters = "";
+        int wrongAnswers= 0;
         
         Scanner scanner = new Scanner(System.in);
         boolean gameIsOn=true;
         while(gameIsOn){
-            
+            boolean guessedCorrect = false;
+
             System.out.println(Arrays.toString(gameWord));
             System.out.println("Guessed Letters: "+guessedLetters);
             System.out.print("Guess: ");
             String guess = scanner.nextLine();
             
             guessedLetters+=guess;
-            
+
             for(int i = 0; i<wordArray.length;i++){
                 if(guess.equals(wordArray[i])){
                     gameWord[i]=guess;
+                    guessedCorrect=true;
                 }
             }
+
+            if(guessedCorrect==false){
+                wrongAnswers++;
+            }
+
+           gameIsOn=CheckWinOrLose(gameWord, gameIsOn, wrongAnswers);
+        
         } 
         
     }
+    public static boolean CheckWinOrLose(String[] gameWord, boolean gameIsOn, int wrongAnswers){
+        if(!Arrays.stream(gameWord).anyMatch("_"::equals)){
+            System.out.println(Arrays.toString(gameWord));
+            System.out.println("You Won!");
+            return false;
+        }
+        else if(wrongAnswers==5){
+            System.out.println("You Lose");
+            return false;
+        }else{
+            return true;
+        }
+    }
+
     public static String[] getBlanks(String word){
         String [] gameWord = new String[word.length()];
         for(int i = 0;i<word.length();i++){
