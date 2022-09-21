9a4481e4496b819a5dbfe32d95476373016d7803
Adolfo Gonzalez <agonzal177@gmail.com>
2022-09-20 22:08:09 -0400

    organized functions and added way to guess letters

--- Hangman.java
+++ Hangman.java
@@ -1,20 +1,48 @@
 package hangman;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Hangman{
     public static void main(String[] args) {
-        double randomDoubleNum = Math.floor(Math.random()*words.length);
-        int randomNum = (int) Math.round(randomDoubleNum);
-        String word = words[randomNum];
+        String word = getWord();
+        System.out.println(word);
+        String[] wordArray= word.split("");
+        String[] gameWord = getBlanks(word);
+        String guessedLetters = "";
         
+        Scanner scanner = new Scanner(System.in);
+        boolean gameIsOn=true;
+        while(gameIsOn){
+            
+            System.out.println(Arrays.toString(gameWord));
+            System.out.println("Guessed Letters: "+guessedLetters);
+            System.out.print("Guess: ");
+            String guess = scanner.nextLine();
+            
+            guessedLetters+=guess;
+            
+            for(int i = 0; i<wordArray.length;i++){
+                if(guess.equals(wordArray[i])){
+                    gameWord[i]=guess;
+                }
+            }
+        } 
+        
+    }
+    public static String[] getBlanks(String word){
         String [] gameWord = new String[word.length()];
         for(int i = 0;i<word.length();i++){
             gameWord[i]="_";
         }
-        System.out.println(Arrays.toString(gameWord));
-        
-        
+        return gameWord;
+    } 
+    public static String getWord(){
+        double randomDoubleNum = Math.floor(Math.random()*words.length);
+        int randomNum = (int) Math.round(randomDoubleNum);
+        String word = words[randomNum];
+        return word;
     }
     public static String[] words = {"ant", "baboon", "badger", "bat", "bear","beaver", "camel", "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer", "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat", "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose", "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", "python", "rabbit", "ram", "rat", "raven","rhino", "salmon", "seal", "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan", "tiger", "toad", "trout", "turkey","turtle", "weasel", "whale", "wolf", "wombat", "zebra"};
 
