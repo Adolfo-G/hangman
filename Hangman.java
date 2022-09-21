fcc88e7ca42efc8868612f80cfe821f2b690c07d
Adolfo Gonzalez <agonzal177@gmail.com>
2022-09-21 13:50:01 -0400

    added animation art and condensed code

--- Hangman.java
+++ Hangman.java
@@ -1,6 +1,5 @@
 package hangman;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -15,9 +14,11 @@ public class Hangman{
         
         Scanner scanner = new Scanner(System.in);
         boolean gameIsOn=true;
+    
         while(gameIsOn){
             boolean guessedCorrect = false;
 
+            System.out.println(gallows[wrongAnswers]);
             System.out.println(Arrays.toString(gameWord));
             System.out.println("Guessed Letters: "+guessedLetters);
             System.out.print("Guess: ");
@@ -25,20 +26,25 @@ public class Hangman{
             
             guessedLetters+=guess;
 
-            for(int i = 0; i<wordArray.length;i++){
-                if(guess.equals(wordArray[i])){
-                    gameWord[i]=guess;
-                    guessedCorrect=true;
-                }
-            }
-
+            guessedCorrect=checkGuess(wordArray, guess, gameWord, wrongAnswers);
             if(guessedCorrect==false){
                 wrongAnswers++;
             }
-
            gameIsOn=CheckWinOrLose(gameWord, gameIsOn, wrongAnswers);
         
         } 
+        scanner.close();
+        
+    }
+    public static boolean checkGuess(String[] wordArray, String guess, String[] gameWord, int wrongAnswers) {
+        boolean isCorrect = false;
+        for(int i = 0; i<wordArray.length;i++){
+            if(guess.equals(wordArray[i])){
+                gameWord[i]=guess;
+                isCorrect= true;
+            }
+        }
+        return isCorrect;
         
     }
     public static boolean CheckWinOrLose(String[] gameWord, boolean gameIsOn, int wrongAnswers){
@@ -47,7 +53,8 @@ public class Hangman{
             System.out.println("You Won!");
             return false;
         }
-        else if(wrongAnswers==5){
+        else if(wrongAnswers==6){
+            System.out.println(gallows[wrongAnswers]);
             System.out.println("You Lose");
             return false;
         }else{
@@ -69,5 +76,60 @@ public class Hangman{
         return word;
     }
     public static String[] words = {"ant", "baboon", "badger", "bat", "bear","beaver", "camel", "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer", "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat", "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose", "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", "python", "rabbit", "ram", "rat", "raven","rhino", "salmon", "seal", "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan", "tiger", "toad", "trout", "turkey","turtle", "weasel", "whale", "wolf", "wombat", "zebra"};
-
+    public static String[] gallows = {
+        "+---+\n" +
+        "|   |\n" +
+        "    |\n" +
+        "    |\n" +   //if the user didn't miss any guesses.
+        "    |\n" +
+        "    |\n" +
+        "=========\n",
+    
+        "+---+\n" +
+        "|   |\n" +
+        "O   |\n" +   //if the user missed one guess.
+        "    |\n" +
+        "    |\n" +
+        "    |\n" +
+        "=========\n",
+    
+        "+---+\n" +
+        "|   |\n" +
+        "O   |\n" +    //if the user missed two guesses.
+        "|   |\n" +
+        "    |\n" +
+        "    |\n" +
+        "=========\n",
+    
+        " +---+\n" +
+        " |   |\n" +
+        " O   |\n" +   //if the user missed three guesses.
+        "/|   |\n" +
+        "     |\n" +
+        "     |\n" +
+        " =========\n",
+    
+        " +---+\n" +
+        " |   |\n" +
+        " O   |\n" +
+        "/|\\  |\n"+   //if the user missed four guesses.
+        "     |\n" +
+        "     |\n" +
+        " =========\n",
+    
+        " +---+\n" +
+        " |   |\n" +
+        " O   |\n" +
+        "/|\\  |\n" +  //if the user missed five guesses.
+        "/    |\n" +
+        "     |\n" +
+        " =========\n",
+    
+        " +---+\n" +
+        " |   |\n" +
+        " O   |\n" +
+        "/|\\  |\n" +   //if the user missed six guesses.
+        "/ \\  |\n" +
+        "     |\n" +
+        " =========\n"};
 }
